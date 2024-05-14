17973430bca3ab86e0e7826f0f6d7a8a82f6f15b
LipeMachadoVezos <felipe.machado@vezos.com.br>
2024-05-14 17:22:42 -0300

    Update NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,60 @@
+
+const navbarFirstList = [
+    {
+        menu : "Página Inicial",
+        link : "/"
+    },
+    {
+        menu : "Nossa História",
+        link : "/"
+    },
+    {
+        menu : "Local",
+        link : "/"
+    },
+]
+
+const navbarSecondList = [
+    {
+        menu : "Presentes",
+        link : "/"
+    },
+    {
+        menu : "Confirmação",
+        link : "/"
+    },
+    {
+        menu : "Contato",
+        link : "/"
+    },
+]
+
 export function NavBar() {
     return (
         <>
             <nav className="h-24 flex justify-center items-center">
                 <ul className="flex w-full gap-20 justify-center items-center text-[#2B1105] text-xl">
-                    <li className="py-2 px-10">Home</li>
-                    <li>Nossa Historia</li>
-                    <li>Local</li>
-                    <div>
-                        <p>Logo</p>
+                    {navbarFirstList.map((item) => (
+                        <a
+                            key={item.menu}
+                            className="text-[#2B1105] hover:text-[#2B1105]/70 transition-colors"
+                            href={item.link}                            
+                        >
+                            {item.menu}
+                        </a>
+                    ))}
+                    <div className="px-10">
+                        <p>F&K</p>
                     </div>
-                    <li>Presentes</li>
-                    <li>Confirmação</li>
-                    <li>Contato</li>
+                    {navbarSecondList.map((item) => (
+                        <a 
+                            key={item.menu}
+                            className="text-[#2B1105] hover:text-[#2B1105]/70 transition-colors"
+                            href={item.link}
+                        >
+                            {item.menu}
+                        </a>
+                    ))}
                 </ul>
             </nav>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { NavBar } from "@/components/NavBar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
