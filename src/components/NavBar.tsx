cd5f3a06e241c93f9814d522bbb556becdd525fb
LipeMachado <silvalipe765@gmail.com>
2024-07-11 00:08:36 -0300

    Add menu hamburguer

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,8 @@
+'use client'
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Hamburger from "hamburger-react";
 
 const navbarFirstList = [
     {
@@ -12,7 +17,7 @@ const navbarFirstList = [
         menu: "Local",
         link: "/"
     },
-]
+];
 
 const navbarSecondList = [
     {
@@ -27,17 +32,23 @@ const navbarSecondList = [
         menu: "Contato",
         link: "/contato"
     },
-]
+];
 
 export function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        if (!menuOpen) {
+            document.body.classList.add('overflow-hidden');
+        } else {
+            document.body.classList.remove('overflow-hidden');
+        }
+    };
+
     return (
         <>
-            <div className="">
-                <div>
-                    hamburguer
-                </div>
-            </div>
-            <nav className="h-36 mb-8 flex justify-center items-center">
+            <nav className="h-full lg:h-36 mb-8 flex justify-evenly lg:justify-center items-center">
                 <ul className="hidden lg:flex-row lg:flex w-full gap-14 justify-center items-center text-[#2B1105] text-lg">
                     {navbarFirstList.map((item) => (
                         <a
@@ -61,12 +72,83 @@ export function NavBar() {
                         </a>
                     ))}
                 </ul>
-                <div className="flex lg:hidden">
-                    <div className="w-24 h-24 flex justify-center items-center border-[1px] border-black rounded-full select-none">
-                        <p className="font-light text-4xl">F&K</p>
+                <div className="w-full flex justify-between md:justify-evenly items-center lg:hidden py-5 px-5">
+                    <div className="flex justify-start items-start h-full">
+                        <button
+                            className="lg:hidden"
+                            onClick={toggleMenu}
+                            aria-label={menuOpen ? "Fechar Menu" : "Abrir Menu"}
+                        >
+                            {/* OPEN MENU MOBILE */}
+                            {menuOpen ? (
+                                <>
+                                    <div className="z-[999] relative">
+                                        <Hamburger toggled={menuOpen} size={20} toggle={setMenuOpen} />
+                                    </div>
+                                    <motion.div
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        transition={{ duration: 0.2 }}
+                                        className="fixed inset-0 mx-auto justify-center items-center content-center bg-[#E9DFF8] bg-opacity-80 backdrop-blur-sm z-50"
+                                    >
+                                        <div className="flex flex-col items-center">
+                                            {navbarFirstList.map((item, idx) => (
+                                                <motion.a
+                                                    initial={{ scale: 0, opacity: 0 }}
+                                                    animate={{ scale: 1, opacity: 1 }}
+                                                    transition={{
+                                                        type: "spring",
+                                                        stiffness: 260,
+                                                        damping: 20,
+                                                        delay: 0.1 + idx / 10,
+                                                    }}
+                                                    key={item.menu}
+                                                    className="text-[#2B1105] hover:text-[#2B1105]/70 transition-colors my-4"
+                                                    href={item.link}
+                                                    onClick={toggleMenu}
+                                                >
+                                                    {item.menu}
+                                                </motion.a>
+                                            ))}
+                                            {navbarSecondList.map((item, idx) => (
+                                                <motion.a
+                                                    initial={{ scale: 0, opacity: 0 }}
+                                                    animate={{ scale: 1, opacity: 1 }}
+                                                    transition={{
+                                                        type: "spring",
+                                                        stiffness: 260,
+                                                        damping: 20,
+                                                        delay: 0.1 + idx / 10,
+                                                    }}
+                                                    key={item.menu}
+                                                    className="text-[#2B1105] hover:text-[#2B1105]/70 transition-colors my-4"
+                                                    href={item.link}
+                                                    onClick={toggleMenu}
+                                                >
+                                                    {item.menu}
+                                                </motion.a>
+                                            ))}
+                                        </div>
+                                    </motion.div>
+                                </>
+                            ) : (
+                                <>
+                                    <div>
+                                        <Hamburger toggled={menuOpen} size={20} toggle={setMenuOpen} />
+                                    </div>
+                                </>
+                            )}
+                        </button>
                     </div>
+                    <div className="flex lg:hidden">
+                        <div className="w-24 h-24 flex justify-center items-center border-[1px] border-black rounded-full select-none">
+                            <p className="font-light text-4xl">K&F</p>
+                        </div>
+                    </div>
+                    <div className="w-[48px] h-[48px]"></div>
                 </div>
             </nav>
         </>
-    )
+    );
 }
